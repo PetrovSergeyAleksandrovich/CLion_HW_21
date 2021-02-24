59f030049a3bd9d99952471c6aa207583217075b
PetrovSergeyAleksandrovich <ps@dsys.ru>
2021-02-24 15:55:22 +0300

    task 1 done

--- HW_21_task_1.cpp
+++ HW_21_task_1.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <vector>
+
+void swap(std::vector<int>& vec, int* ary)
+{
+    for (int i = 0, tmp = 0; i < vec.size(); i++)
+    {
+        tmp = vec[i];
+        vec[i] = ary[i];
+        ary[i] = tmp;
+    }
+}
 
 int main()
 {
-    std::cout << "hello world";
+    std::vector<int> my_vector = {1, 2, 3, 4, 5};
+    int my_array[] = {2, 4, 6, 8, 10};
+
+    swap(my_vector, my_array);
+
+    for(int i = 0; i < my_vector.size(); ++i)
+    {
+        std::cout << my_vector[i] << " ";
+    }
+
+    std::cout << std::endl;
+
+    for(int i = 0; i < sizeof(my_array)/sizeof(int); ++i)
+    {
+        std::cout << my_array[i] << " ";
+    }
+
 }
 
