c53a231b39153ce09d673a1acafce57347a22706
PetrovSergeyAleksandrovich <ps@dsys.ru>
2021-02-26 16:50:02 +0300

    task 4 - single '0' count done

--- HW_21_task_4.cpp
+++ HW_21_task_4.cpp
@@ -2,14 +2,14 @@
 
 void evendigits(long long number, int& k)
 {
-    if (number < 1) return;
-    if (number%2==0 && number%10!=0) k++;
+    if (number%2==0 || number%10==0) k++;
+    if (number <= 2) return;
     evendigits(number/10, k);
 }
 
 int main()
 {
-    long long number = 120108; /* your number to check*/
+    long long number = 0; /* your number to check*/
     int res = 0;
     evendigits(number, res);
     std::cout << "evendigits quantity: " << res;
