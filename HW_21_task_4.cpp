bb6d2f998411cc071bb022780bed8741111ab28f
PetrovSergeyAleksandrovich <ps@dsys.ru>
2021-02-26 16:16:31 +0300

    task 4 - count even digits without '0' done

--- HW_21_task_4.cpp
+++ HW_21_task_4.cpp
@@ -3,13 +3,13 @@
 void evendigits(long long number, int& k)
 {
     if (number < 1) return;
-    if (number%2 == 0) k++;
+    if (number%2==0 && number%10!=0) k++;
     evendigits(number/10, k);
 }
 
 int main()
 {
-    long long number = 123456789101112; /* your number to check*/
+    long long number = 120108; /* your number to check*/
     int res = 0;
     evendigits(number, res);
     std::cout << "evendigits quantity: " << res;
